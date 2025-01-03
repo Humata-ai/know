67b8921951877e059ab1deb7054a24cb041d1968
Dan Rasmuson <danrasmuson@gmail.com>
2025-01-03 15:19:20 -0600

    WIP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,17 @@ import React from "react";
 import { createEmbeddings } from "@/utils-server/openai/create-embedding";
 
 export default async function Home() {
-  const embeddings = await createEmbeddings(['cat'], { dimensions: 3 })
+  const [{ embedding: catEmbedding }] = await createEmbeddings(['cat'])
+  const [{ embedding: catEmbeddingThree }] = await createEmbeddings(['cat'], { dimensions: 3 })
 
   return (
     <div>
-      embeddings: {JSON.stringify(embeddings)}
+      <div>
+        cat (dimensions {catEmbedding.length})<span style={{ marginLeft: '54px' }}></span>: {JSON.stringify(catEmbedding).slice(0, 200)}...
+      </div>
+      <div>
+        cat (reduced to 3 dimensions): {JSON.stringify(catEmbeddingThree)}
+      </div>
     </div>
   );
 }
